a06fc5e13de90f36e7f3e08fd9d0ada25cf30aed
MK <muratkaraoz@gmail.com>
2021-06-21 21:48:01 +0200

    Update mfr.py
    minor bug fixes

--- mfr.py
+++ mfr.py
@@ -1,82 +1,56 @@
-import textwrap
 import frida
-import frida.core
 import argparse
 
-# about rpc method calls: https://github.com/frida/frida-python/issues/104
+parser = argparse.ArgumentParser(prog='mfr')
+parser.add_argument('app', help='app package name')
+parser.add_argument('-f', '--find', type=str, required=True, help='text to be search')
+parser.add_argument('-r', '--replace', type=str, required=True, help='text to be written')
+arguments = parser.parse_args()
 
-#arg parser
-def getParams():
-    parser = argparse.ArgumentParser(
-        prog='text-search',
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description=textwrap.dedent(""))
+PACKAGE_NAME = arguments.app
+FIND = arguments.find
+FIND_HEX_ENCODED = FIND.encode().hex()
+REPLACE = arguments.replace
 
-    parser.add_argument('process',
-                        help='the process that you will be injecting to')
-    parser.add_argument('-f', '--find', type=str, required=True,
-                        help='text that will be replaced')
-    parser.add_argument('-r', '--replacement', type=str, required=True,
-                        help='text that will replace old text')
-    args = parser.parse_args()
-    return args
+session = frida.get_usb_device().attach(PACKAGE_NAME)
 
-def on_message(message, data):
-    print("[on_message] message:", message, "data:", data)
-
-arguments = getParams()
-APP_NAME = arguments.process
-OLD_TEXT = arguments.find
-# If the new text is longer than the old text writeUtf8String() may
-# cause a buffer-overflow and may corrupt unrelated memory.
-NEW_TEXT = arguments.replacement
-PERMS = 'w' 
-
-session = None
-try:
-    session = frida.get_usb_device().attach(APP_NAME)
-    print("connecte to process " + APP_NAME)
-except Exception as e:
-    print("Can't connect to App. Have you connected the device?")
-    logging.debug(str(e))
-    sys.exit()
-
-print("Starting memory search...")
-
+#create the JS snippet
 script = session.create_script(
-    """'use strict';
+    """
     rpc.exports = {
-      enumerateRanges: function (prot) {
-        return Process.enumerateRangesSync(prot);
+      enumerateRanges (protection) {
+        return Process.enumerateRangesSync(protection);
       },
 
-      searchMemory: function (address, size, pattern) {
+      searchMemory (address, size, pattern) {
         return Memory.scanSync(ptr(address), size, pattern);
       },
 
-      writeToMemory: function (address, text) {
+      writeToMemory (address, text) {
         var np = new NativePointer(address);
         np.writeUtf8String(text);
       }
-     };
+    };
     """)
 
-script.on("message", on_message)
 script.load()
+rpc = script.exports
 
-agent = script.exports
-ranges = agent.enumerate_ranges(PERMS)
+ranges = rpc.enumerate_ranges("rw-")
 
-# Performing the memory search
+#This counter will keep track of number of changes.
 counter = 0;
 for range in ranges:
-    base = range["base"]
-    size = range["size"]
-    results = agent.search_memory(base, size, OLD_TEXT.encode("utf-8").hex())
+    base = range["base"] 
+    size = range["size"] 
+    results = rpc.search_memory(base, size, FIND_HEX_ENCODED)
+
     for result in results:
         memAddress = result["address"]
-        agent.write_to_memory(memAddress, NEW_TEXT)
-        print("@" + base + " replaced " + OLD_TEXT + " with " + NEW_TEXT)
-        counter=counter+1;
+        #At each iteration, calls writeToMemory funtion
+        rpc.write_to_memory(memAddress, REPLACE)
+        print("@" + memAddress + " " + FIND + " was replaced with " + REPLACE + ".")
+        counter = counter + 1;
 
-print ("Replaced " + str(counter) + " strings.");
+#Show the user how many changes are made
+print (str(counter) + " changes are made.");
